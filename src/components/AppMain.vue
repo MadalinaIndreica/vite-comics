f421605bcef14b110de76408c5bd528d9b355703
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2023-11-23 17:52:27 +0100

    add: icons in main section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,7 +12,29 @@ export default {
         <h3> &LeftArrow; content goes here &rightarrow;</h3>
     </section>
     <section class=" main-section blue-section">
-    
+     <div class="main-icons container ">
+        <div>
+            <img class="smaller" src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <h4>DIGITAL COMICS</h4>
+        </div>
+        <div>
+            <img class="normal" src="../assets/img/buy-comics-merchandise.png" alt="">
+            <h4>DC MERCHANDISE</h4>
+        </div>
+        <div>
+            <img class="normal" src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <h4>SUBSCRIPTION</h4>
+        </div>
+        <div>
+            <img  class="smaller" src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <h4>COMIC SHOP LOCATOR</h4>
+        </div>
+        <div>
+            <img  class="normal" src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <h4>DC POWER VISA</h4>
+        </div>
+
+     </div>
     </section>
     
 </template>
@@ -27,6 +49,23 @@ export default {
 }
 .blue-section {
     background-color: $primary;
+    .main-icons {
+        padding: 3rem;
+        @include  flex();
+        justify-content: space-between;
+        div {
+            @include  flex();
+
+        }
+       .normal {
+        width: 50px;
+            
+       }
+       .smaller {
+                width: 30px;
+            }
+       
+    }
   
 }
 
